6cd13b27df310c93d94c583790deb0da48e751ff
Yigit Can <y.can.bolukbasi@gmail.com>
2021-12-09 13:11:48 +0300

    Ayrı style dosyası Input için oluşturuldu

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {View, StyleSheet, TextInput, Text} from 'react-native';
-import spacing from '../../styles/spacing';
-import radius from '../../styles/radius';
+import styles from './Input.style';
 
 export default function Input({label, ...otherProps}) {
   return (
@@ -14,18 +13,4 @@ export default function Input({label, ...otherProps}) {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    margin: spacing.large,
-  },
-  input_container: {
-    backgroundColor: '#e0e0e0',
-    padding: spacing.normal,
-    borderRadius: radius.sharp,
-    borderWidth: 1,
-    borderColor: '#bdbdbd',
-  },
-  label: {
-    color: '#8d8d8d',
-  },
-});
+
