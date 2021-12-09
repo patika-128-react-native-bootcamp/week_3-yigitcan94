934f39103cfda20faf8551799307aac5136e5eb1
Yigit Can <y.can.bolukbasi@gmail.com>
2021-12-09 13:10:51 +0300

    Gerekli güncellemeler yapıldı

--- android/app/src/main/java/com/week_3/MainApplication.java
+++ android/app/src/main/java/com/week_3/MainApplication.java
@@ -1,7 +1,9 @@
 package com.week_3;
-
+ 
 import android.app.Application;
 import android.content.Context;
+import com.facebook.react.bridge.JSIModulePackage; // <- add
+import com.swmansion.reanimated.ReanimatedJSIModulePackage; // <- add
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactInstanceManager;
@@ -33,6 +35,10 @@ public class MainApplication extends Application implements ReactApplication {
         protected String getJSMainModuleName() {
           return "index";
         }
+        @Override
+      protected JSIModulePackage getJSIModulePackage() {
+        return new ReanimatedJSIModulePackage(); // <- add
+      }
       };
 
   @Override
